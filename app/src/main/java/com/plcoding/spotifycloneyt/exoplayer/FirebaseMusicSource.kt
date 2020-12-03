4e64949dbe18e343fb5536b1975b47dd892bdd4c
Ivan Milic <milic.tg@gmail.com>
2020-12-03 12:19:02 +0100

    Finished whenReady source

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,43 @@
+package com.plcoding.spotifycloneyt.exoplayer
+
+import com.plcoding.spotifycloneyt.exoplayer.State.*
+
+class FirebaseMusicSource {
+
+    //receives a list of functions that accept booleans and return unit
+    //check the state of firebase
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = STATE_CREATED
+        set(value) {
+            if (value == STATE_INITIALIZED || value == STATE_ERROR) {
+                //only access from one thread
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        return if (state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListeners += action
+            false
+        } else {
+            action(state == STATE_INITIALIZED)
+            true
+        }
+    }
+
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -2,4 +2,5 @@ package com.plcoding.spotifycloneyt.other
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+    const val SERVICE_TAG = "MusicService"
 }
