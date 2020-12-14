155eb1fcdee642958c2721f7fda5877158ad85c5
Ivan Milic <milic.tg@gmail.com>
2020-12-14 11:54:31 +0100

    Finished Prepering music player

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
@@ -3,11 +3,14 @@ package com.plcoding.spotifycloneyt.exoplayer
 import android.app.PendingIntent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlaybackPreparer
+import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerEventListener
 import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.plcoding.spotifycloneyt.other.Constants.SERVICE_TAG
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,6 +29,9 @@ class MusicService : MediaBrowserServiceCompat() {
     @Inject
     lateinit var exoPlayer: SimpleExoPlayer
 
+    @Inject
+    lateinit var firebaseMusicSource: FirebaseMusicSource
+
     private lateinit var musicNotificationManager: MusicNotificationManager
 
     //service is not async
@@ -38,6 +44,8 @@ class MusicService : MediaBrowserServiceCompat() {
     private lateinit var mediaSessionConnector: MediaSessionConnector
 
     var isForegroundService = false
+
+    private var currentPlayingSong: MediaMetadataCompat? = null
 
     override fun onCreate() {
         super.onCreate()
@@ -62,8 +70,21 @@ class MusicService : MediaBrowserServiceCompat() {
 
         }
 
+        val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
+            currentPlayingSong = it
+            preparePlayer(
+                firebaseMusicSource.songs,
+                it,
+                true
+            )
+        }
+
         mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlaybackPreparer(musicPlaybackPreparer)
         mediaSessionConnector.setPlayer(exoPlayer)
+
+        exoPlayer.addListener(MusicPlayerEventListener(this))
+        musicNotificationManager.showNotification(exoPlayer)
     }
 
     override fun onDestroy() {
@@ -84,6 +105,17 @@ class MusicService : MediaBrowserServiceCompat() {
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
         TODO("Not yet implemented")
+    }
+
+    private fun preparePlayer(
+        songs: List<MediaMetadataCompat>,
+        itemToPlay: MediaMetadataCompat?,
+        playNow: Boolean
+    ) {
+        val currentSongIndex = if (currentPlayingSong == null) 0 else songs.indexOf(itemToPlay)
+        exoPlayer.prepare(firebaseMusicSource.asMediaSource(dataSourceFactory))
+        exoPlayer.seekTo(currentSongIndex, 0L)
+        exoPlayer.playWhenReady = playNow
     }
 
 }
--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/callbacks/MusicPlaybackPreparer.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/callbacks/MusicPlaybackPreparer.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.ResultReceiver
 import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import com.google.android.exoplayer2.ControlDispatcher
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
@@ -19,29 +20,23 @@ class MusicPlaybackPreparer(
         command: String,
         extras: Bundle?,
         cb: ResultReceiver?
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
 
     override fun getSupportedPrepareActions(): Long {
-        TODO("Not yet implemented")
+        return PlaybackStateCompat.ACTION_PREPARE_FROM_MEDIA_ID or PlaybackStateCompat.ACTION_PLAY_FROM_MEDIA_ID
     }
 
-    override fun onPrepare(playWhenReady: Boolean) {
-        TODO("Not yet implemented")
-    }
+    override fun onPrepare(playWhenReady: Boolean) = Unit
 
     override fun onPrepareFromMediaId(mediaId: String, playWhenReady: Boolean, extras: Bundle?) {
-        TODO("Not yet implemented")
+        firebaseMusicSource.whenReady {
+            val itemToPlay = firebaseMusicSource.songs.find {
+                mediaId == it.description.mediaId
+            }
+            playerPrepared(itemToPlay)
+        }
     }
 
-    override fun onPrepareFromSearch(query: String, playWhenReady: Boolean, extras: Bundle?) {
-        TODO("Not yet implemented")
-    }
+    override fun onPrepareFromSearch(query: String, playWhenReady: Boolean, extras: Bundle?) = Unit
 
-    override fun onPrepareFromUri(uri: Uri, playWhenReady: Boolean, extras: Bundle?) {
-        TODO("Not yet implemented")
-    }
-
-
+    override fun onPrepareFromUri(uri: Uri, playWhenReady: Boolean, extras: Bundle?) = Unit
 }
