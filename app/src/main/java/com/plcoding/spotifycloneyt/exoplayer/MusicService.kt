2004602cf0f0817fe579d228f9f70c3075a00f65
MilicTG <milic.tg@gmail.com>
2020-12-17 22:07:30 +0100

    Music service connection

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/MusicService.kt
@@ -17,6 +17,7 @@ import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlaybackPreparer
 import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerEventListener
 import com.plcoding.spotifycloneyt.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.plcoding.spotifycloneyt.other.Constants.MEDIA_ROOT_ID
+import com.plcoding.spotifycloneyt.other.Constants.NETWORK_ERROR
 import com.plcoding.spotifycloneyt.other.Constants.SERVICE_TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
@@ -143,6 +144,7 @@ class MusicService : MediaBrowserServiceCompat() {
                             isPlayerInitialized = true
                         }
                     } else {
+                        mediaSession.sendSessionEvent(NETWORK_ERROR, null)
                         result.sendResult(null)
                     }
                 }
