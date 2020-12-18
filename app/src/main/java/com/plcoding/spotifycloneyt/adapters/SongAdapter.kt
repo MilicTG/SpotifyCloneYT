75384fe51c4bcd64d7e78c3706debf53d6567464
MilicTG <milic.tg@gmail.com>
2020-12-18 13:19:08 +0100

    Finish Adapters

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/adapters/SongAdapter.kt
@@ -0,0 +1,47 @@
+package com.plcoding.spotifycloneyt.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.plcoding.spotifycloneyt.data.entity.Song
+import javax.inject.Inject
+
+class SongAdapter @Inject constructor(
+    private val glide: RequestManager
+) : RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
+
+    class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Song>() {
+        override fun areItemsTheSame(oldItem: Song, newItem: Song): Boolean {
+            return oldItem.mediaId == newItem.mediaId
+        }
+
+        override fun areContentsTheSame(oldItem: Song, newItem: Song): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    var songs: List<Song>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
