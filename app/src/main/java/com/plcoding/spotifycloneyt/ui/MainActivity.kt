365cfa7a5f00befca86f24a6fca74ab11821027c
Ivan Milic <milic.tg@gmail.com>
2020-12-26 14:13:23 +0100

    BaseSongAdapter

--- app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
@@ -2,19 +2,45 @@ package com.plcoding.spotifycloneyt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.bumptech.glide.RequestManager
 import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.adapters.SwipeSongAdapter
+import com.plcoding.spotifycloneyt.data.entity.Song
+import com.plcoding.spotifycloneyt.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val mainViewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var swipeSongAdapter: SwipeSongAdapter
+
     @Inject
     lateinit var glide: RequestManager
+
+    private var currentlyPlayingSong: Song? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vpSong.adapter = swipeSongAdapter
+    }
+
+    private fun switchViewPagerToCurrentSong(song: Song){
+        val newItemIndex = swipeSongAdapter.songs.indexOf(song)
+        if (newItemIndex != -1){
+            vpSong.currentItem = newItemIndex
+            currentlyPlayingSong = song
+        }
+    }
+
+    private fun subscribeToObservers(){
+        
     }
 }
